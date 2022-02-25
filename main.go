d5fe41787f767c7c4a516b372b8efcd5b02ffd1e
James <53650971+jmstudyacc@users.noreply.github.com>
2022-02-25 21:53:58 +0000

    adding custom router & default case

--- main.go
+++ main.go
@@ -8,13 +8,37 @@ import (
 )
 
 // creating a function to handle the http request & present a web page
-func handlerFunc(writer http.ResponseWriter, reader *http.Request) {
+func homeHandler(writer http.ResponseWriter, reader *http.Request) {
+	// defines the Content-Type header to be used >> Content Type Headers - HTTP Headers ~ Mozilla
+	writer.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(writer, "<h1>Welcome to the awesome site!</h1>")
 }
 
+func contactHandler(writer http.ResponseWriter, reader *http.Request) {
+	fmt.Fprint(writer, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jmstudyacc@gmail.com\">jmstudyacc@gmail.com</a>")
+}
+
+// implementing a custom router
+func pathHandler(writer http.ResponseWriter, reader *http.Request) {
+	// implement a switch statement to capture the options
+	switch reader.URL.Path {
+	case "/":
+		homeHandler(writer, reader)
+	case "/contact":
+		contactHandler(writer, reader)
+	default:
+		// this returns a 404 courtesy of the NotFound function
+		//http.NotFound(writer, reader)
+		// this allows you to customise the response
+		http.Error(writer, "Error 404\nPage Not Found - Did you mean something else?", http.StatusNotFound)
+	}
+}
+
 func main() {
+	http.HandleFunc("/", pathHandler)
 	// registration - pass in a pattern to determine the paths to be used by the handler and a function for the handler
-	http.HandleFunc("/", handlerFunc)
+	//http.HandleFunc("/", homeHandler)
+	//http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the webserver on :3000")
 	// starts the server & prevents the code from exiting
 	err := http.ListenAndServe(":3000", nil)
