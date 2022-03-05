c307e3ab9f9a30f2ffde140afb8a511da38dd06d
James <53650971+jmstudyacc@users.noreply.github.com>
2022-03-05 23:05:36 +0000

    adding a template and .gohtml example

---
+++ cmd/exp/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	// ensure you have html/template package for text when running a template
+	"html/template"
+	"os"
+)
+
+type User struct {
+	Name string
+	Age  int
+	Meta UserMeta
+}
+
+type UserMeta struct {
+	Visits int
+}
+
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	user := User{
+		Name: "John Smith",
+		Age:  99,
+		Meta: UserMeta{Visits: 5},
+	}
+
+	// this is using an anonymous struct and not creating a type struct
+	// this is useful when you want to test something quickly - useful when you are running test functions
+	//user2 := struct {
+	//	Name string
+	//}{
+	//	Name: "Susan Smith",
+	//}
+
+	// you have to define where the template is output to
+	err = t.Execute(os.Stdout, user)
+
+	if err != nil {
+		panic(err)
+	}
+}
